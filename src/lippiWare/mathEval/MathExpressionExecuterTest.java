07d91c37f2a66bd4449ec67ce9fbbb51e1a69dce
Gabor Liptak <liptakgabor@freemail.hu>
2023-03-04 19:50:35 +0100

    Further test cases Test code simplification

--- src/lippiWare/mathEval/MathExpressionExecuterTest.java
+++ src/lippiWare/mathEval/MathExpressionExecuterTest.java
@@ -4,6 +4,7 @@ import lippiWare.mathEval.MathExpressionParser;
 public class MathExpressionExecuterTest {
 
     static void t(String expression, int expectedResult) {
+        System.out.println("expression=" + expression);
         MathExpressionExecuterBase meeb = new MathExpressionExecuterBase();
         MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", expression);
         int result = mep.executeInt();
@@ -12,53 +13,19 @@ public class MathExpressionExecuterTest {
             throw new Error("Invalid result is calculated!");
     }
 
-    static void t0() {
-        MathExpressionExecuterBase meeb = new MathExpressionExecuterBase();
-        //MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7");
-        MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5");
-        System.out.println("mep=" + mep.getExpression() + " result = " + mep.executeInt());
-    }
-
-    static void t1() {
-        MathExpressionExecuterBase meeb = new MathExpressionExecuterBase();
-        //MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7");
-        MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "-5");
-        System.out.println("mep=" + mep.getExpression() + " result = " + mep.executeInt());
-    }
-
-    static void t2() {
-        MathExpressionExecuterBase meeb = new MathExpressionExecuterBase();
-        //MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7");
-        MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7");
-        System.out.println("mep=" + mep.getExpression() + " result = " + mep.executeInt());
-    }
-
-    static void t3() {
-        MathExpressionExecuterBase meeb = new MathExpressionExecuterBase();
-        //MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7");
-        MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "-5+7");
-        System.out.println("mep=" + mep.getExpression() + " result = " + mep.executeInt());
-    }
-
-    static void t4() {
-        MathExpressionExecuterBase meeb = new MathExpressionExecuterBase();
-        //MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7");
-        MathExpressionParser mep = new MathExpressionParser(meeb, "varResult", "5+7+3");
-        System.out.println("mep=" + mep.getExpression() + " result = " + mep.executeInt());
-    }
-
     public static void main(String[] args) {
-        t0();
-        t1();
-        t2();
-        t3();
-        t4();
+        t("5", 5);
+        t("-5", -5);
+        t("5+7", 12);
+        t("-5+7", 2);
+        t("5+7+3", 15);
         t("3+2-2-3", 0);
         t("3*2", 6);
         t("3*2+3", 9);
         t("2+3*2", 8);
         t("2-3*2", -4);
         t("2+3*2+4", 12);
+        t("3*2+3*2", 12);
+        t("3 * 2 + 3 * 2", 12);
     }
-
 }
